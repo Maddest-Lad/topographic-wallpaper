e4f8e1b9ed354065283ab0cb619c12708f2a24b6
Maddest-Lad <samharris23@live.com>
2026-02-10 20:06:17 -0800

    fix: build setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/endfield-wallpaper/',
+  base: '/Endfield-Wallpaper/',
   plugins: [react(), tailwindcss()],
 })
