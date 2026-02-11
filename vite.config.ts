bb0265ffe72536f24efc61ffddcba6e0e254c865
Maddest-Lad <samharris23@live.com>
2026-02-10 16:49:25 -0800

    feat: setup for github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/endfield-wallpaper/',
   plugins: [react(), tailwindcss()],
 })
