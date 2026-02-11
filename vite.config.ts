8383e2f8156b5ebb026580d8ecebe6f3c51230d3
Maddest-Lad <samharris23@live.com>
2026-02-10 22:44:28 -0800

    chore: update name

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/Endfield-Wallpaper/',
+  base: '/topographic-wallpaper/',
   plugins: [react(), tailwindcss()],
 })
