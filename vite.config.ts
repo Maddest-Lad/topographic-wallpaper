ab15596031a87d77bcb72efc43033c2a7c4ffb0e
Maddest-Lad <samharris23@live.com>
2026-02-11 09:47:51 -0800

    feat: update branding

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/topographic-wallpaper/',
+  base: '/fieldgrid/',
   plugins: [react(), tailwindcss()],
 })
